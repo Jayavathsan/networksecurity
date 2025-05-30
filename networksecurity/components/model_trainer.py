27d7ee81ff0aee11677b3e8810d39a80f92ee24b
Jayavathsan <jayavathsan@gmail.com>
2025-05-30 15:55:04 +0530

    testing the full ci, cd

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -23,6 +23,7 @@ from networksecurity.utils.main_utils.utils import save_object,load_object,load_
 from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
 
 
+# if this needs to be initialized for container then add the secrets otherwise it wont work or show any error
 # dagshub.init(repo_owner='jayavathsan', repo_name='networksecurity', mlflow=True)
 
 
